b37902158af48f49944ab815ff112e6369d21db5
krzysiek9209 <krzysiek9209@gmail.com>
2022-10-23 13:23:17 +0200

    wip

--- word_permutations/test_permutations.py
+++ word_permutations/test_permutations.py
@@ -4,5 +4,5 @@ from .permutations import get_permutations
 def test_permutations():
     test_case = "abc"
     permutations = get_permutations(test_case)
-    expected_result = ["abc", "acb", "bac", "bca", "cab", "cba"]
+    expected_result = ["abc", "acb", "bac", "bca", "cab", "cba", "ddd"]
     assert set(permutations) == set(expected_result)
